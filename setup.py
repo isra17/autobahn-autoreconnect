97371ee47420b25712c1d39defe2cadfb72d8d51
isra17 <isra017@gmail.com>
2015-07-25 00:00:08 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='autobahn-autoreconnect',
+    version='0.0.1',
+
+    description='Python Autobahn runner with auto-reconnect feature',
+    url='https://github.com/isra17/autobahn-autoreconnect',
+    author='isra17',
+    author_email='isra017@gmail.com',
+    license='LGPL2',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=['autobahn']
+)
