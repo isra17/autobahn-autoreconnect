3e54f8996249b1b9c6b582fdd225542cc047b8f1
isra17 <isra017@gmail.com>
2015-08-23 21:55:42 -0400

    Remove git dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='autobahn-autoreconnect',
-    version='0.0.1',
+    version='0.0.2',
 
     description='Python Autobahn runner with auto-reconnect feature',
     url='https://github.com/isra17/autobahn-autoreconnect',
@@ -11,6 +11,5 @@ setup(
     license='LGPL2',
 
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    install_requires=['autobahn==0.10.5'],
-    dependency_links=['https://github.com/tavendo/AutobahnPython/archive/3bcbc00382a9d601fe4565216d4e7dc737d5f65e.zip#egg=autobahn-0.10.5']
+    install_requires=['autobahn>=0.10.5']
 )
